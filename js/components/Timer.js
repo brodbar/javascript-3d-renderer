28fc7d31164353326964063a403ce778554717d7
brodbar <83711894+brodbar@users.noreply.github.com>
2022-05-05 02:30:08 +0300

    Fixed timer causing freeze after some time

--- js/components/Timer.js
+++ js/components/Timer.js
@@ -1,20 +1,37 @@
 export class Timer {
     constructor(deltaTime = 1/60) {
-        let lastTime;
-        let accumulatedTime = 0;
-        this.queued = false;
+        this.lastTime;
+        this.accumulatedTime = 0;
+        this.tick = 0;
 
         this.update = (time) => {
-            if(lastTime) {
-                accumulatedTime += (time - lastTime) / 1000;
-                while(accumulatedTime > deltaTime) {
+            if(this.lastTime) {
+                this.accumulatedTime += (time - this.lastTime) / 1000;
+                while(this.accumulatedTime > deltaTime) {
                     this.callback(deltaTime);
-                    accumulatedTime -= deltaTime;
+                    this.accumulatedTime -= deltaTime;
+                    this.tick++;
+                    if(this.tick > 150)
+                    {
+                        this.pause();
+                        return;
+                    }
                 }
             }
-            lastTime = time;
+            this.lastTime = time;
             this.enqueue();
         }
+
+        ["keydown", "mousemove", "mousedown"].forEach(e => {
+            window.addEventListener(e, () => {
+                this.tick = 0;
+                if(this.paused)
+                {
+                    this.paused = false;
+                    this.start();
+                }
+            });
+        });
     }
 
     evoke(callback) {
@@ -23,13 +40,17 @@ export class Timer {
     }
 
     enqueue() {
-        if (this.queued !== false)
-            this.queued = requestAnimationFrame(this.update);
+        requestAnimationFrame(this.update);
+    }
+    
+    pause()
+    {
+        this.lastTime = 0;
+        this.accumulatedTime = 0;
+        this.paused = true;
     }
 
     start() {
-        this.queued = true;
-        this.enqueue();
+        this.enqueue();        
     }
-
 }
--- js/main.js
+++ js/main.js
@@ -10,12 +10,13 @@ const Graph = {
     Scene,
     Equation: function() {
         return {
-            x: Math.sin(this.parameter),
+            x: Math.sin(this.parameter), //(1 + 0.25*cos(75t))*cost
             y: Math.cos(this.parameter),
-            z: Math.sin(this.parameter) * Math.cos(this.parameter)
+            z: Math.cos(this.parameter) * Math.sin(this.parameter)
         }
     },
-    parameter: 0
+    parameter: 0,
+    points: []
 }
 
 const cursorLock = [
@@ -23,16 +24,21 @@ const cursorLock = [
 ].some(e => e in document);
 
 const _Camera = new Camera(Graph.Scene, cursorLock);
-_Camera.setPos(5, 0, 0)
+_Camera.setPos(10, 0, 0);
+
+for(let i = 0; i < 1000; i++)
+{
+    Graph.points.push(Graph.Equation())
+    Graph.parameter += Math.PI / 288;
+}
 
 function draw()
 {
     _Camera.handleInput();
-    Graph.Scene.context.clearRect(0, 0, Screen.width, Screen.height)
-    for(let i = 0; i < 10e2; i++)
+    Graph.Scene.context.clearRect(0, 0, Screen.width, Screen.height);
+    for(let point of Graph.points)
     {
-        _Camera.sprayPoint(Graph.Equation());
-        Graph.parameter += Math.PI / 144;
+        _Camera.sprayPoint(point);
     }
 }
 
